69b9c8c4562c38470d267d715f06ec7c31b3c02d
Ali Zeeshan <alizeeshanch91@gmail.com>
2026-08-09 20:32:17 +0500

    basic physics in Arm.h

--- Arm.h
+++ Arm.h
@@ -1,1 +1,15 @@
 #pragma once
+struct Arm
+{
+	double angle;
+	double angularvelocity;
+
+	Arm(double angle, double angularVelocity, double lenght, double mass);
+	Arm();
+	double getLength() const;
+	double getMass() const;
+
+private:
+	double length;
+	double mass;
+};
