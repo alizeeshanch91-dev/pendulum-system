7a189c774fe6783783628bb6ff2261d6fad7f15e
Ali Zeeshan <alizeeshanch91@gmail.com>
2026-08-10 00:57:26 +0500

    missing values added in Arm.cpp

--- Arm.cpp
+++ Arm.cpp
@@ -7,11 +7,15 @@ Arm::Arm(double angle, double angularVelocity, double lenght, double mass)
 	assert(mass > 0);
 	this->angle = angle;
 	this->angularvelocity = angularVelocity;
+	this->mass = mass;
+	this->length = length;
 }
 Arm::Arm()
 {
 	angle = 0;
 	angularvelocity = 0;
+	length = 1;
+	mass = 1;
 }
 double Arm::getLength() const
 {
