7a117d772da3a29554956e8e05e665875235b5ff
Ali Zeeshan <alizeeshanch91@gmail.com>
2026-08-10 00:29:52 +0500

    added physics in Pendulum.cpp

--- Pendulum.cpp
+++ Pendulum.cpp
@@ -1,4 +1,5 @@
 #include "Pendulum.h"
+#include <cmath>
 
 Pendulum::Pendulum(Arm arm1, Arm arm2, double pivotX, double pivotY)
 {
@@ -33,8 +34,43 @@ double Pendulum::getPivotY()
 {
 	return pivotY;
 }
-void Pendulum::update()
+void Pendulum::update(double dt,double gravity)
 {
+	double L1 = arm1.getLength();
+	double m1 = arm1.getMass();
+	double L2 = arm2.getLength();
+	double m2 = arm2.getMass();
+	double theta1 = arm1.angle;
+	double theta2 = arm2.angle;
+	double omega1 = arm1.angularvelocity;
+	double omega2 = arm2.angularvelocity;
+	double delta = theta1 - theta2;
+	double M = 2*arm1.getMass() + arm2.getMass();
+	double denom1 = L1 * (2 * m1 + m2 - m2 * cos(2 * delta));
+	double denom2 = L2 * (2 * m1 + m2 - m2 * cos(2 * delta));
+	// A: primary gravity torque
+	double A = -gravity * M * sin(theta1);
+	// B: second gravity effect
+	double B = -m2 * gravity * sin(theta1 - 2 * theta2);
+	// C: tug torque
+	double C = -2 * sin(delta) * m2 * (omega2 * omega2 * L2 + omega1 * omega1 * L1 * cos(delta));
+	// angular acceleration of the first arm
+	double angular_acc_1 = (A + B + C) / denom1;
+
+	// D: drive acceleration from arm 1
+	double D = omega1 * omega1 * L1 * (m1 + m2);
+	// E: base gravity transmition
+	double E = gravity * (m1 + m2) * cos(theta1);
+	// F: arm 2 speed feedback
+	double F = omega2 * omega2 * L2 * m2 * cos(delta);
+	// angular accerlation of the second arm
+	double angular_acc_2 = (2 * sin(delta) * (D + E + F)) / (denom2);
+
+	// semi-implicit Euler integration: velocity first, then angle
+	arm1.angularvelocity += angular_acc_1 * dt;
+	arm2.angularvelocity += angular_acc_2 * dt;
+	arm1.angle += arm1.angularvelocity * dt;
+	arm2.angle += arm2.angularvelocity * dt;
 }
 void Pendulum::draw()
 {
