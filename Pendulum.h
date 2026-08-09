4971e10708148e976bdefcda73a4683323777722
Ali Zeeshan <alizeeshanch91@gmail.com>
2026-08-10 03:17:56 +0500

    added text on rendering screen Pendulum.h

--- Pendulum.h
+++ Pendulum.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Arm.h"
-
+#include "raylib.h"
 class Pendulum
 {
 private:
@@ -8,13 +8,14 @@ private:
 	Arm arm2;
 	double pivotX;
 	double pivotY;
+	Color armColor;
 public:
-	Pendulum(Arm arm1, Arm arm2, double pivotX, double pivotY);
+	Pendulum(Arm arm1, Arm arm2, double pivotX, double pivotY,Color armColor);
 	Pendulum();
 	Arm getArm2();
 	Arm getArm1();
 	double getPivotX();
 	double getPivotY();
 	void update(double dt, double gravity);
-	void draw();
+	void draw(int textX);
 };
