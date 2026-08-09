38280c5e9f8c4d0b0d428ae6998bd51595a5829c
Ali Zeeshan <alizeeshanch91@gmail.com>
2026-08-09 20:31:07 +0500

    added some content and getters in Pendulum.h

--- Pendulum.h
+++ Pendulum.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Arm.h"
+
+class Pendulum
+{
+private:
+	Arm arm1;
+	Arm arm2;
+	double pivotX;
+	double pivotY;
+public:
+	Pendulum(Arm arm1, Arm arm2, double pivotX, double pivotY);
+	Pendulum();
+	Arm getArm2();
+	Arm getArm1();
+	double getPivotX();
+	double getPivotY();
+	void update();
+	void draw();
+};
